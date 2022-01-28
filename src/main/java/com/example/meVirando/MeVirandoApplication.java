56bf8388e7f2c444a38f9d7f6ed7dd2de9f401b5
Tony Branquinho <tony.branquinho@gmail.com>
2022-01-28 11:29:28 -0300

    first commit

---
+++ src/main/java/com/example/meVirando/MeVirandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.meVirando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeVirandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeVirandoApplication.class, args);
+	}
+
+}
